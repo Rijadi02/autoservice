9428c271c14cdfb6b0a1a1bd0b4398a93e5e839a
venismorina <venismorinax8@gmail.com>
2021-07-18 02:57:46 +0200

    cars not car shih

--- database/migrations/2021_07_11_151138_create_cars_table.php
+++ database/migrations/2021_07_11_151138_create_cars_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateCarTable extends Migration
+class CreateCarsTable extends Migration
 {
     /**
      * Run the migrations.
@@ -24,6 +24,7 @@ class CreateCarTable extends Migration
             $table->integer('year')->nullable();
             $table->string('fuel')->nullable();
             $table->float('weight')->nullable();
+            $table->string('images')->nullable();
             $table->integer('client_id')->unsigned();
             $table->foreign('client_id')->references('id')->on("clients")->onDelete("cascade");
 
